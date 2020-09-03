ba4f1a922138af16e45bb656caf2bdab4bbe033d
luisfranciscobecerra <luisfranciscobecerra09@gmail.com>
2020-09-03 09:19:15 -0400

    integración a Firebase Analytics

--- app/src/main/java/com/example/fincabectronix/MainActivity.kt
+++ app/src/main/java/com/example/fincabectronix/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fincabectronix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,10 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this) //llama a Firebase Analytics y lo guarda en una constante
+        val bundle = Bundle() //crea un bundle
+        bundle.putString("message", "Integración de Firebase completa" ) //clave y valor para el bundle
+        analytics.logEvent("InitScreen", bundle)  //llama a la función logEvent (clave, parametros) y lanza el evento. La clave de la función es InitScreen y los parametros los pasamos a traves del bundle
     }
 }
